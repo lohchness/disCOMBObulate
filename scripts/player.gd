7d89b710d02eb2da0022ff14d44e5b3f663900d8
Lok/Tom Chow <lohchness@gmail.com>
2025-01-19 15:14:58 +1100

    Player state chart with movement and dash

---
+++ scripts/player.gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+
+const MOVE_SPEED = 750
+const MOVE_ACCEL = 350
+
+
+@onready var statechart = $StateChart
+@export var dashcurve: Curve
+
+var direction: Vector2
+var is_moving: bool
+
+## Dashing
+var is_dashing: bool = false
+var dash_cooldown: float = 3         # Time to wait after dashes are spent
+var can_dash = true
+
+# Dash speed and DashTimer wait_time affects distance
+@onready var dash_timer = $DashTimer
+const DASH_SPEED = 3000
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
+
+# Polling (single key presses)
+func _on_basic_state_input(event: InputEvent) -> void:
+	# Dashing supercedes all movement
+	if event.is_action_pressed("dash") and can_dash:
+		$StateChart.send_event("on_dash")
+
+func _on_idle_state_physics_processing(delta: float) -> void:
+	move_and_slide()
+	if Input.get_vector("left", "right", "up", "down"):
+		statechart.send_event("on_move")
+
+func _on_run_state_physics_processing(delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+	velocity.x = move_toward(velocity.x, MOVE_SPEED * direction.x, MOVE_ACCEL)
+	velocity.y = move_toward(velocity.y, MOVE_SPEED * direction.y, MOVE_ACCEL)
+	move_and_slide()
+	
+	is_moving = velocity.length_squared() >= 0.005
+
+	if not is_moving:
+		$StateChart.send_event("on_idle")
+
+## Dashing
+
+func _on_dash_timer_timeout() -> void:
+	can_dash = true
+	statechart.send_event("finish_dash")
+
+func _on_dash_state_entered() -> void:
+	#if direction == Vector2.ZERO:
+		#$StateChart.send_event("finish_dash")
+	
+	dash_timer.start()
+	can_dash = false
+	print(direction)
+
+func _on_dash_state_physics_processing(delta: float) -> void:
+	velocity = direction * roll_speed(dash_timer.wait_time-dash_timer.time_left) * DASH_SPEED
+	
+	move_and_slide()
+
+func roll_speed(t: float):
+	return dashcurve.sample(t)
