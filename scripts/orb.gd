dc70211e3e7bcac29d3d044954fd7008eb8a43f8
Lok/Tom Chow <lohchness@gmail.com>
2025-01-21 19:36:51 +1100

    Orb scene. Magnetizes to player magent and pickup radius

---
+++ scripts/orb.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var travel_to_player = false
+var player: Player = null
+var time_since_magnet: float = 0
+
+func _physics_process(delta: float) -> void:
+	if travel_to_player:
+		position = position.lerp(player.global_position, 8 * delta * (1 + time_since_magnet))
+		time_since_magnet += delta
+		
+
+func _on_magnet_area_entered(area: Area2D) -> void:
+	if area.is_in_group("PlayerMagnetRadius"):
+		travel_to_player = true
+		player = area.get_parent().get_parent()
+
+
+func _on_pickup_area_entered(area: Area2D) -> void:
+	if area.is_in_group("PlayerPickupRadius"):
+		queue_free()
